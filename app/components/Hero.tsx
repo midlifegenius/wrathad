7d70df6805c61c1ff9c1132a66f89af3ef8f913a
midlifegenius <robmp3@gmail.com>
2026-03-17 13:55:43 -0700

    logo flick

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+
 import SocialLinks from './SocialLinks'
 import VideoSection from './VideoSection'
 
@@ -9,33 +9,22 @@ export default function Hero() {
   return (
     <div className='flex flex-col items-center text-center'>
       {/* Logo */}
-      <motion.div
-        initial={{ opacity: 0, scale: 0.9 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-        className='mb-8'
-      >
+      <div className='mb-8 relative'>
         <Image
           src='/logo2.png'
           alt='Wrath A.D.'
           width={500}
           height={250}
-          className='object-contain'
+          className='object-contain wrath-logo'
           priority
         />
-      </motion.div>
+      </div>
 
       {/* Tagline 
-      <motion.p
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.4 }}
-        className='text-xs tracking-[0.4em] text-gray-400 mb-6 uppercase'
-      >
+      <p className='text-xs tracking-[0.4em] text-gray-400 mb-6 uppercase'>
         Heavy. Relentless. Uncompromising.
-      </motion.p>
-*/}
-      {/* Links NOW directly under */}
+      </p>*/}
+
       <SocialLinks />
       <VideoSection />
     </div>
