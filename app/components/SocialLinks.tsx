25943e4572a330a4c7639e90f10aa2ca6db0da84
midlifegenius <robmp3@gmail.com>
2026-03-17 13:34:17 -0700

    good social

--- app/components/SocialLinks.tsx
+++ app/components/SocialLinks.tsx
@@ -1,26 +1,52 @@
 'use client'
 
+import { FaSpotify, FaYoutube, FaInstagram, FaApple } from 'react-icons/fa'
+
 const links = [
-  { name: 'Spotify', url: 'https://spotify.com' },
-  { name: 'Apple Music', url: 'https://music.apple.com' },
-  { name: 'YouTube', url: 'https://youtube.com' },
-  { name: 'Instagram', url: 'https://instagram.com' },
+  {
+    name: 'Spotify',
+    url: 'https://open.spotify.com/artist/5axlid9Cok4er7nJKTDi6I',
+    icon: FaSpotify,
+    hover: 'hover:text-green-500',
+  },
+  {
+    name: 'Apple Music',
+    url: 'https://music.apple.com/us/artist/wrath-a-d/1870893700',
+    icon: FaApple,
+    hover: 'hover:text-gray-300',
+  },
+  {
+    name: 'YouTube',
+    url: 'https://www.youtube.com/@wrathad',
+    icon: FaYoutube,
+    hover: 'hover:text-red-500',
+  },
+  {
+    name: 'Instagram',
+    url: 'https://instagram.com',
+    icon: FaInstagram,
+    hover: 'hover:text-pink-500',
+  },
 ]
 
 export default function SocialLinks() {
   return (
-    <div className='flex gap-6 text-xs tracking-[0.3em] uppercase text-gray-500'>
-      {links.map((link) => (
-        <a
-          key={link.name}
-          href={link.url}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='hover:text-white transition'
-        >
-          {link.name}
-        </a>
-      ))}
+    <div className='flex gap-6 mt-2'>
+      {links.map((link) => {
+        const Icon = link.icon
+
+        return (
+          <a
+            key={link.name}
+            href={link.url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={`text-gray-500 ${link.hover} hover:scale-110 transition duration-300`}
+          >
+            <Icon size={22} />
+          </a>
+        )
+      })}
     </div>
   )
 }
